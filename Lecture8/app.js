229d10823e11fc111c57210974ebd4f7d8580150
Rohan <rohanb459@gmail.com>
2024-01-23 12:01:45 +0530

    lecture9 completed

--- Lecture8/app.js
+++ Lecture8/app.js
@@ -2,6 +2,8 @@
     angular.module("DIApp", [])
     .controller("DIController", DIController);
 
+    DIController.$inject = ["$scope", "$filter"];
+
     function DIController($scope, $filter)
     {
         $scope.name="Rohan"
