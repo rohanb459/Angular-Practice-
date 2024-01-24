fc75ea08cbe5f8c3a482079c48b69be0e9218413
Rohan <rohanb459@gmail.com>
2024-01-24 09:59:31 +0530

    Lecture 12 Completed

--- Lecture11/app.js
+++ Lecture11/app.js
@@ -1,16 +1,41 @@
 (function(){
     angular.module("myApp", [])
     .controller("myController", myController)
+    .filter("loves", lovesFilter)
 
-    myController.$inject = ["$scope"];
+    myController.$inject = ["$scope", "lovesFilter"];
 
-    function myController($scope)
+    function myController($scope, lovesFilter)
     {
         $scope.picNumber = "1";
+        $scope.cost = .50
         $scope.changePicture=function()
         {
             if($scope.picNumber==="1")$scope.picNumber="2";
             else $scope.picNumber="1";
+        }
+
+        $scope.sayMessage = function ()
+        {
+            var msg = "Rohan likes to go to gym!"
+            return msg;
+        }
+
+        $scope.sayLovesMessage = function ()
+        {
+            var msg = "Rohan likes to go to gym!"
+            msg=lovesFilter(msg); 
+            return msg;
+        }
+    }
+
+    function lovesFilter()
+    {
+        return function (input)
+        {
+            input = input || "";
+            input = input.replace("likes", "loves");
+            return input;
         }
     }
 })();
